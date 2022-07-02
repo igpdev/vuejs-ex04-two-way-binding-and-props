b3833eb24c66945b30099f6492d64ebce66f5e44
igpdev <iguerrero.dev@gmail.com>
2022-07-02 01:31:44 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vuejs-ex04-two-way-binding-and-props-solved
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vuejs-ex04-filters)
---
+++ src/Parent.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <!-- EXERCISE 04: Two-way data binding -->
+    <h1>Ex 04: Two-way Data binding</h1>
+    Available users:
+    <li v-for="user in users">{{ user.name }}</li>
+    <br />
+    Search a user age: <input v-model="userToSearch" />
+    <p>
+      User entered <strong>{{ userToSearch }}</strong>
+    </p>
+    <br />
+    <Child @fetchUsers="fetchUsersService" :userToSearch="userToSearch" />
+  </div>
+</template>
+
+<script>
+import Child from './Child.vue';
+
+export default {
+  name: 'App',
+  data: () => ({
+    userToSearch: null,
+    users: [
+      { name: 'Mario', age: 29 },
+      { name: 'David', age: 40 },
+      { name: 'John', age: 39 },
+    ],
+  }),
+  components: {
+    Child,
+  },
+  methods: {
+    async fetchUsersService(callback) {
+      const response = await this.fakeCall();
+      callback(this.users);
+    },
+    fakeCall() {
+      return new Promise((resolve) => {
+        setTimeout(function () {
+          resolve();
+        }, 2000);
+      });
+    },
+  },
+};
+</script>
+
+<style></style>
